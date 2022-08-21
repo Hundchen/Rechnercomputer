0dde58fadda4fff08594cd74b10a11f95d397f26
David Fischer <dajofischer@gmail.com>
2022-08-21 11:44:44 +0200

    first commit

---
+++ code.py
@@ -0,0 +1,16 @@
+from time import sleep
+
+# print(64109876543102344332406886665467773)
+
+# print(10)
+
+anfangszahl = 100
+endzahl = 1
+wieschnell = 0
+
+while anfangszahl > endzahl:
+    anfangszahl = anfangszahl- 1
+    sleep(wieschnell)
+    print(anfangszahl)
+
+ 
